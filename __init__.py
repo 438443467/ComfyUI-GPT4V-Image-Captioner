8c8bc216ee601b6f6c7bfdba06530d25fb1498ef
438443467 <132811983+438443467@users.noreply.github.com>
2024-03-15 00:51:58 +0800

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,6 +1,13 @@
 from .GPT4V_captioner import NODE_CLASS_MAPPINGS as styClassMappings, NODE_DISPLAY_NAME_MAPPINGS as styDisplay
+from .Load_Image_Batch import NODE_CLASS_MAPPINGS as utilClassMappings, NODE_DISPLAY_NAME_MAPPINGS as utilDisplay
+
+
 NODE_CLASS_MAPPINGS = styClassMappings
 # 定义模块的公开接口
+NODE_CLASS_MAPPINGS = {**styClassMappings, **utilClassMappings}
+
+NODE_DISPLAY_NAME_MAPPINGS = {**styDisplay, **utilDisplay}
+
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
 
 
